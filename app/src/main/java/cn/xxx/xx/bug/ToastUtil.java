8e7494b0888a6d17ebe5bbcefbde2cf147fa927d
root <674753084@qq.com>
2017-09-01 22:33:18 +0800

    加入URL编码函数

--- app/src/main/java/cn/xxx/xx/bug/ToastUtil.java
+++ app/src/main/java/cn/xxx/xx/bug/ToastUtil.java
@@ -3,7 +3,10 @@ package cn.xxx.xx.bug;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -40,5 +43,47 @@ public class ToastUtil {
         }
 
         return hex.toString();
+    }
+
+    //判断文件是否存在
+    public static boolean fileIsExists(String strFile) {
+        try {
+            File f = new File(strFile);
+            if (!f.exists()) {
+                return false;
+            }
+
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static String toURLEncoded(String paramString) {
+        if (paramString == null || paramString.equals("")) {
+            return "";
+        }
+        try {
+            String str = new String(paramString.getBytes(), "UTF-8");
+            str = URLEncoder.encode(str, "UTF-8");
+            return str;
+        } catch (Exception localException) {
+        }
+        return "";
+    }
+
+    public static String toURLDecoded(String paramString) {
+        if (paramString == null || paramString.equals("")) {
+            return "";
+        }
+        try {
+            String str = new String(paramString.getBytes(), "UTF-8");
+            str = URLDecoder.decode(str, "UTF-8");
+            return str;
+        } catch (Exception localException) {
+        }
+
+        return "";
     }
 }
