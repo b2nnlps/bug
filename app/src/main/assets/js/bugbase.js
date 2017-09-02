3f1adabc4d36ffbeede9d716d292a46d278db895
root <674753084@qq.com>
2017-09-02 22:07:07 +0800

    加入了图标，和关于网页

--- app/src/main/assets/js/bugbase.js
+++ app/src/main/assets/js/bugbase.js
@@ -1,6 +1,24 @@
 ﻿function print(){
-    window.android.print("bbbbbbbb");//重载默认HTML系统函数
+    var data=document.getElementsByTagName('html')[0].innerHTML;
+    window.android.print(data);//重载默认HTML系统函数
+    console.log("打印网页内容");
 }
 function print(str){
     window.android.print(str);//传入打印数据
 }
+
+function bugCom(){
+    com=getQueryString("bugCom");
+    if(com=="login") bugLogin();
+}
+
+
+function getQueryString(name) { //获取get传过来的用户信息
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return decodeURI(r[2]);
+    return "";
+}
+
+console.log("七星虫JS执行中");
+bugCom();
